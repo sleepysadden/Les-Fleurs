f99f361c9d7223fbc627bf39d0b917d1a2d603b2
sleepysadden <zboralskib@gmail.com>
2025-05-11 23:28:09 +0200

    Initial commit

---
+++ Pack/overrides/kubejs/server_scripts/recipes.js
@@ -0,0 +1,28 @@
+ServerEvents.recipes(event => {
+    // Addition:
+    
+    
+    
+    //Modification:
+    
+    
+    //Removal:
+    event.remove({ id: 'lolmbv:bookshelf' });
+    event.remove({ id: 'createfood:minecraft/crafting/waffle_batter_bucket_from_crafting' });
+    event.remove({ id: 'createfood:minecraft/crafting/cheesecaske_filling_bucket_from_crafting' });
+    event.remove({ id: 'createfood:minecraft/crafting/cream_cheese_bucket_from_crafting' });
+    event.remove({ id: 'createfood:minecraft/crafting/sour_cream_bucket_from_crafting' });
+    event.remove({ id: 'createfood:minecraft/crafting/yogurt_bucket_from_crafting' });
+    event.remove({ id: 'createfood:minecraft/crafting/fruit_smoothie_bucket_from_crafting' });
+    event.remove({ id: 'createfood:minecraft/crafting/fruit_smoothie_bucket_from_crafting_alt' });
+    event.remove({ id: 'createfood:minecraft/crafting/melon_jam_bucket_from_crafting' });
+    event.remove({ id: 'createfood:minecraft/crafting/melon_milkshake_bucket_from_crafting' });
+    event.remove({ id: 'createfood:minecraft/crafting/melon_milkshake_bucket_from_crafting_alt' });
+    event.remove({ id: 'createfood:minecraft/crafting/melon_milkshake_bucket_from_crafting_alt_2' });
+    event.remove({ id: /^createfood:minecraft/crafting/.*_bucket_from_crafting' });
+
+
+
+    
+        console.log('Recipes modified by KubeJS');
+    });
